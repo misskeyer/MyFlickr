44bc0296050f06404c2df3eb0d01d73e8b58e1a6
Chars.L <tree0key@gmail.com>
2018-12-30 14:33:45 +0800

    Fix bugs that Android 8.0 notifications don't show

--- app/src/main/java/com/example/chars/photocollection/PollService.java
+++ app/src/main/java/com/example/chars/photocollection/PollService.java
@@ -1,14 +1,17 @@
 package com.example.chars.photocollection;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -20,6 +23,7 @@ import java.util.List;
 
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
+    private static final String CHANNEL_ID = "channel_pollservice";
     private static final int POLL_INTERVAL = 1000 * 60;
 
     public PollService() {
@@ -79,8 +83,16 @@ public class PollService extends IntentService {
             Resources resources = getResources();
             Intent i = photoCollectionActivity.newIntent(this);
             PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            Notification notification = null;
 
-            Notification notification = new NotificationCompat.Builder(this)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            @SuppressLint("WrongConstant") NotificationChannel channel = new NotificationChannel(CHANNEL_ID, TAG,
+                    NotificationManagerCompat.IMPORTANCE_LOW);
+            Log.i(TAG,"Channel created.");
+            manager.createNotificationChannel(channel);
+
+            notification = new NotificationCompat.Builder(this , CHANNEL_ID)
                     .setTicker(resources.getString(R.string.new_photos_title))
                     .setSmallIcon(android.R.drawable.ic_menu_report_image)
                     .setContentTitle(resources.getString(R.string.new_photos_title))
@@ -88,9 +100,18 @@ public class PollService extends IntentService {
                     .setContentIntent(pi)
                     .setAutoCancel(true)
                     .build();
+            } else {
+                notification = new NotificationCompat.Builder(this)
+                        .setTicker(resources.getString(R.string.new_photos_title))
+                        .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                        .setContentTitle(resources.getString(R.string.new_photos_title))
+                        .setContentText(resources.getString(R.string.new_photos_text))
+                        .setContentIntent(pi)
+                        .setAutoCancel(true)
+                        .build();
+            }
 
-            NotificationManagerCompat notificationmanager = NotificationManagerCompat.from(this);
-            notificationmanager.notify(0, notification);
+            manager.notify(0, notification);
         }
         QueryPreferences.setLastResultId(this, resultId);
     }
